dd93bcbd1aed8abfaeb8e82913eab4a922b49b4d
lcslima45 <lcslima45@gmail.com>
2025-07-08 14:52:18 -0300

    Update

--- channels/main.go
+++ channels/main.go
@@ -6,12 +6,34 @@ func hello(done chan bool) {
 	fmt.Println("eu sou uma nova goroutine")
 	done <- true
 }
+
+func hello2(done chan<- bool) {
+	fmt.Println("eu sou uma goroutine com chan direcional")
+	done <- true
+}
+
+func numeros(v chan<- int) {
+	for i := 0; i < 10; i++ {
+		v <- i
+	}
+	close(v)
+}
+
 func main() {
 	done := make(chan bool)
-
+	c := make(chan int)
+	go numeros(c)
 	go hello(done)
 
 	<-done
 
-	fmt.Println("eu sou a goroutine principal")
+	done2 := make(chan bool)
+	go hello2(done2)
+	<-done2
+
+	for v := range c {
+		fmt.Println("valor:", v)
+	}
+
+	fmt.Println("eu sou a goro utine principal")
 }
